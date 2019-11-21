7df3f219881a26e2e1faea6ea2a91e11277d1af3
Bumper0417 <chrisgoros13@gmail.com>
2019-11-21 10:35:58 +0000

    updates

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
-#sdfsdflksdjflj
+
 
 def add_message(username, message):
     """Add messages to the `messages` list"""
@@ -37,6 +37,6 @@ def user(username):
     return render_template("chat.html", username=username, chat_messages=messages)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', 
-    port=5000, 
+    app.run(host=os.environ.get("IP"), 
+    port=int(os.environ.get("PORT")), 
     debug=False)
